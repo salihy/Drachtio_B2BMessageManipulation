8a027304c1c40f498a3520f2e427063839f697c0
Dave Horton <daveh@beachdognet.com>
2018-07-05 08:44:27 -0400

    initial changes to simplify, handle multiple REFERs in sequence

--- app.js
+++ app.js
@@ -1,86 +1,69 @@
-'use strict';
-
 const config = require('config');
 const Srf = require('drachtio-srf');
-const parseUri = require('drachtio-sip').parser.parseUri;
-//const parser = require('drachtio-sip').parser;
-var referArray = [];
-
 const srf = new Srf();
 
 srf.connect(config.get('drachtio'))
-    .on('connect', (err, hostport) => {
-        if (err) {
-            return console.error(`error connecting: ${err.message}`);
-        }
-        console.log(`connected hostport: ${hostport}`);
-    });
-
+  .on('connect', (err, hostport) => {
+    if (err) return console.error(`error connecting: ${err.message}`);
+    console.log(`connected hostport: ${hostport}`);
+  });
 
 srf.register((req, res) => {
-    res.send(200);
+  res.send(200);
+});
+
+// example use of middleware to authenticate and stuff like that
+srf.use('invite', (req, res, next) => {
+  console.log(`incoming invite from ${req.source_address} with uri ${req.uri}`);
+  if (!config.get('sipservers').some((w) => w.ip == req.source_address)) {
+    return res.send(580, 'Sip Server Is Not Authenticated!');
+  }
+  next();
 });
 
 srf.invite((req, res) => {
+  // at this point we have an authenticated request
+  const uri = `${config.get('destination')};transport=udp`;
+  srf.createB2BUA(req, res, uri)
+    .then(({ uac, uas }) => {
+      console.log(`call connected, initial call-ids ${uas.sip.callId} and ${uac.sip.callId}`);
 
+      // we always have a pair of dialogs, so lets have each have a ref to the other
+      uas.other = uac;
+      uac.other = uas;
+      setHandlers([uac, uas]);
+      return;
 
-
-    if (config.get('sipservers').filter(w => w.ip == req.source_address).length == 0) {
-        res.send(580, "Sip Server Is Not Authenticated!");
-        return;
-    }
-
-    let inviteSent;
-    const uri = `${config.get('destination')};transport=udp`;
-
-    srf.on('request', (x) => {
-        console.log(JSON.stringify(x));
+    })
+    .catch((err) => {
+      console.log(`Error connecting call: ${err}`);
     });
-
-    srf.createB2BUA(req, res, uri)
-        .then(({ uac, uas }) => {
-            console.log('connected');
-
-            uas.on('refer', (requas, resuas) => {
-                resuas.send(202, "Refer Accepted");
-
-                //var referUri = parseUri(requas.headers['refer-to']);
-
-                referArray.push(requas.headers["call-id"]);
-
-                srf.createUAC(requas.headers['refer-to'].replace('<sip:', '').replace('>', ''), { localSdp: req.msg.body })
-                    .then((referuac) => {
-                        console.log(`dialog established, call-id is ${referuac.sip.callId}`);
-                        //uac.on('destroy', () => console.log('called party hung up'));
-
-                        uac.modify(referuac.req.body);
-                        uas.destroy();
-                        return;
-                    })
-                    .catch((err) => {
-                        console.error(`INVITE rejected with status: ${err}`);
-                        console.log(err.stack);
-                    });
-            });
-            uac.on('refer', (requac, resuac) => {
-                resuac.send(202, "Refer Accepted");
-            });
-
-            uac.on('destroy', () => {
-                uas.destroy();
-            });
-            uas.on('destroy', (msg) => {
-                if (referArray.includes(msg.headers["call-id"])) {
-                    referArray = referArray.filter(f => f != msg.headers["call-id"]);
-                }
-                else {
-                    uac.destroy();
-                }
-                
-            });
-        })
-        .catch((err) => {
-            console.log(`Error connecting call: ${err.message}`);
-        });
-
 });
+
+function setHandlers(dialogs) {
+  dialogs = Array.isArray(dialogs) ? dialogs : [dialogs];
+
+  dialogs.forEach((dlg) => {
+    dlg
+      .on('destroy', () => dlg.other.destroy())
+      .on('modify', (req, res) => {
+        dlg.other.modify(req.body)
+          .then(() => res.send(200, {body: dlg.other.remote.sdp}))
+          .catch((err) => console.log(`Error handling reinvite: ${err}`));
+      })
+      .on('refer', handleRefer.bind(dlg));
+  });
+}
+function handleRefer(req, res) {
+  console.log(`handling refer on ${req.get('Call-ID')}`);
+  const uri = req.get('refer-to').replace('<sip:', '').replace('>', '');
+  return srf.createUAC(uri, {localSdp: this.other.remote.sdp})
+    .then((dlg) => {
+      console.log(`Connected new call leg on ${dlg.sip.callId}`);
+      dlg.other = this.other;
+      this.other.other = dlg;
+      this.destroy();
+      setHandlers(dlg);
+      return;
+    });
+}
